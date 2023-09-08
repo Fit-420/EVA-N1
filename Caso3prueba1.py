cfdfbb1925287874819319675970493851ac9f27
NetAcad DEVASC <changeme@example.com>
2023-09-08 13:21:14 +0000

    Evaluación N°1 DEVNET

--- Caso3prueba1.py
+++ Caso3prueba1.py
@@ -8,29 +8,45 @@ precios_sandwiches = {
 
 # Ingreso de la cantidad de sándwiches por teclado
 sandwiches_cliente = {}
+print("¡Bienvenido a Sanguchitos!")
+print("Por favor, ingrese la cantidad de cada sándwich que desea pedir:")
+
 for tipo in precios_sandwiches:
-    cantidad = int(input(f"Ingrese la cantidad de sándwiches {tipo}: "))
+    cantidad = int(input(f"{tipo}: "))
     sandwiches_cliente[tipo] = cantidad
 
 # Cálculo del total
 total = sum(precios_sandwiches[tipo] * cantidad for tipo, cantidad in sandwiches_cliente.items())
 
 # Pregunta si desea entrega a domicilio
-entrega_domicilio = input("¿Desea entrega a domicilio? (Sí/No): ").strip().lower()
+print("\n¿Desea entrega a domicilio? (Sí/No): ", end="")
+entrega_domicilio = input().strip().lower()
 
 if entrega_domicilio == "si" or entrega_domicilio == "sí":
     total += 1500
-    direccion_entrega = input("Por favor, ingrese la dirección de entrega: ")
+    comuna = input("Ingrese la comuna de entrega: ")
+    calle = input("Ingrese la calle de entrega: ")
+    numero_casa = input("Ingrese el número de casa de entrega: ")
+    direccion_entrega = f"{comuna}, {calle} #{numero_casa}"
 else:
     direccion_entrega = "No aplica"
 
 # Descuento (si aplica)
-codigo_descuento = input("Ingrese el código de descuento (o deje en blanco si no tiene): ")
+codigo_descuento = input("\nIngrese el código de descuento (o deje en blanco si no tiene): ")
 if codigo_descuento == "CEBOLLA":
     descuento = 0.10 * total
     total -= descuento
-    print(f"Se aplicó un descuento del 10%: -${descuento:.2f}")
+    descuento_texto = f"Descuento del 10%: -${descuento:.2f}"
+else:
+    descuento_texto = "No aplica"
 
-# Mostrar el total a pagar y la información de entrega
-print(f"Total a pagar: ${total:.2f}")
-print(f"Dirección de entrega: {direccion_entrega}")
+# Imprimir recibo
+print("\n*** Recibo de Compra ***")
+print("-" * 30)
+for tipo, cantidad in sandwiches_cliente.items():
+    subtotal = precios_sandwiches[tipo] * cantidad
+    print(f"{tipo:<15} x{cantidad:<5} ${subtotal:.2f}")
+print("-" * 30)
+print(f"Entrega a domicilio: {direccion_entrega}")
+print(f"{descuento_texto:<20} Total: ${total:.2f}")
+print("-" * 30)
